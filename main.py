6aeed91e7090d92438b4b2e8d078e337532cdfd2
RayShapiro <46408626+RayShapiro@users.noreply.github.com>
2020-12-18 20:20:18 -0500

    first cut at wave images

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 tiles.set_tilemap(tilemap("""level"""))
 
 def on_update_interval():
-    myTiles.tile1.flipY()
+    myTiles.tile1.flipX()
     myTiles.tile2.flipX()
 game.on_update_interval(500, on_update_interval)
--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 tiles.setTilemap(tilemap`level`)
 game.onUpdateInterval(500, function on_update_interval() {
-    myTiles.tile1.flipY()
+    myTiles.tile1.flipX()
     myTiles.tile2.flipX()
 })
