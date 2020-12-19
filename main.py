d1c2ae10bbff6a2b5a4664e858a528c32d0ab33f
RayShapiro <46408626+RayShapiro@users.noreply.github.com>
2020-12-19 10:32:42 -0500

    Clean up and document

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
-
-
+# left looking shark is False
 my_flipped = False
 
 tiles.set_tilemap(tilemap("""level"""))
 
+# animate water
 def on_update_interval():
-    console.log_value("z", my_flipped)
     myTiles.tile1.flipX()
     myTiles.tile2.flipX()
 game.on_update_interval(500, on_update_interval)
 
+# load shark images to tile array
 mytiles = [sprites.builtin.shark0, 
             sprites.builtin.shark1, 
             sprites.builtin.shark2,
             sprites.builtin.shark3]
 
+# setup shark player
 my_sprite = sprites.create(sprites.builtin.shark0, SpriteKind.player)
 scene.camera_follow_sprite(my_sprite)
+
+# set controller to only control up down, l/r buttons control left & right
 controller.move_sprite(my_sprite,0)
 my_sprite.set_velocity(-15, 0)
 
+# animate
 animation.run_image_animation(my_sprite, mytiles, 300, True)
 
+# buttons, flip images and set velocity for direction
 def on_button_r():
     global my_flipped
 
--- main.ts
+++ main.ts
@@ -1,16 +1,22 @@
+//  left looking shark is False
 let my_flipped = false
 tiles.setTilemap(tilemap`level`)
+//  animate water
 game.onUpdateInterval(500, function on_update_interval() {
-    console.logValue("z", my_flipped)
     myTiles.tile1.flipX()
     myTiles.tile2.flipX()
 })
+//  load shark images to tile array
 let mytiles = [sprites.builtin.shark0, sprites.builtin.shark1, sprites.builtin.shark2, sprites.builtin.shark3]
+//  setup shark player
 let my_sprite = sprites.create(sprites.builtin.shark0, SpriteKind.Player)
 scene.cameraFollowSprite(my_sprite)
+//  set controller to only control up down, l/r buttons control left & right
 controller.moveSprite(my_sprite, 0)
 my_sprite.setVelocity(-15, 0)
+//  animate
 animation.runImageAnimation(my_sprite, mytiles, 300, true)
+//  buttons, flip images and set velocity for direction
 controller.player1.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Pressed, function on_button_l() {
     
     if (my_flipped == false) {
